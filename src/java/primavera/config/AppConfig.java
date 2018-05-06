3571cf35cfdcbe4f80dcc8db56174a062d0a88a5
Wagner Macedo <wagnerluis1982@gmail.com>
2018-05-06 11:09:27 -0300

    Configurando a conexão ao banco de dados
    --
    
    Driver JDBC do SQL Server
    
    1. Baixe a biblioteca em http://central.maven.org/maven2/com/microsoft/sqlserver/mssql-jdbc/6.4.0.jre8/mssql-jdbc-6.4.0.jre8.jar
    2. Dentro do NetBeans, selecione o menu _Ferramentas > Bibliotecas_.
    3. Na janela que aparece, clique em _Nova Biblioteca..._.
    4. Escreva para o nome da biblioteca "Driver JDBC do SQL Server" e
       clique em _OK_.
    5. Na tela que aparece, clique em _Adicionar JAR/Pasta..._, escolha o
       arquivo baixado e depois clique em _OK_.
    
    Fazendo o driver como dependência do projeto
    
    1. No projeto NetBeans, clique no botão direito em _Bibliotecas_, no
       menu, selecione _Adicionar Biblioteca..._.
    2. Na janela que aparece, escolha "Driver JDBC do SQL Server" e
       finalize.

--- src/java/primavera/config/AppConfig.java
+++ src/java/primavera/config/AppConfig.java
@@ -5,10 +5,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebMvc
@@ -31,6 +35,23 @@ public class AppConfig {
         messageSource.setDefaultEncoding("UTF-8");
         messageSource.setUseCodeAsDefaultMessage(true);
         return messageSource;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+
+        ds.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        ds.setUrl("jdbc:sqlserver://localhost;databaseName=PJW");
+        ds.setUsername("sa");
+        ds.setPassword("123456");
+
+        return ds;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
     }
 
 }
